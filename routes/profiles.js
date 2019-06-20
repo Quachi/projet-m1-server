968a41180e37081f9d3018517d58548e8c23152f
Marcus++ <jeannet.marc@gmail.com>
2019-06-20 13:29:50 +0200

    cleanup & adding modify profile route w/ pic

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,13 +1,21 @@
 const express = require("express")
-const Profile = require("../models/profile")
-const passport = require("passport")
 const jwt = require("jsonwebtoken")
+const passport = require("passport")
+const multer = require("multer")
+
+const Profile = require("../models/profile")
 const config = require("../config/database")
 
 const router = express.Router()
-
-const projections = {_id: 0, username: 1, avatar: 1, description: 1, posts: 1, comments: 1}
-const putOptions = {new: true, useFindAndModify: false}
+const upload = multer({
+    fileFilter: (req, file, cb) => {
+        switch(file.mimetype) {
+            case "image.png": cb(null, true); break
+            case "image.jpeg": cb(null, true); break
+            default: cb(new Error("Wrong type !")); break
+        }
+    }
+})
 
 router.post("/register", (req, res, next) => {
     Profile.findOne({mail: req.body.mail}, (err, profile) => {
@@ -19,6 +27,7 @@ router.post("/register", (req, res, next) => {
         res.status(200).send({success: true, message: "Profile created !"})
     })
 })
+
 
 router.post("/login", (req, res, next) => {
     Profile.findOne({username: req.body.username}, (err, profile) => {
@@ -37,33 +46,35 @@ router.post("/login", (req, res, next) => {
     })
 })
 
-router.get("/logout", (req, res, next) => { res.status(200).send("logout") })
 
 router.get("/me", passport.authenticate("jwt", {session: false}), (req, res, next) => {
     ["_id", "password"].forEach(element => delete req.user[element])
     res.status(200).send(req.user)
 })
 
-router.put("/me", passport.authenticate("jwt", {session: false}), (req, res, next) => {
-    const id = req.body.id
-    if(id != req.user.id) { return res.status(403).send({error: "Wrong id"}) }
-    ["_id", "id", "username", "password", "mail", "posts", "comments", "historyPosts", "historyComments"].forEach(element => delete req.body[element])
-    Profile.findOneAndUpdate({id: id}, {$set: req.body}, putOptions, (err, profile) => {
-        if(err) { return res.status(500).send() }
-        return res.status(204).send(profile)
+router.put("/me", passport.authenticate("jwt", {session: false}), upload.single("avatar"), (req, res, next) => {
+    const randomid = () => [...Array(64)].map(i => (~~(Math.random()*36)).toString(36)).join("")
+    const img = `data:${req.file.mimetype};base64,${req.file.buffer.toString("base64")}`
+    async.waterfall([
+        callback => {
+            if(!img) { callback(null, req.body) }
+            const media = new Media({id: randomid, data: img})
+            media.save((err, media) => {
+                if(err) { callback(err, null) }
+                req.body.avatar = media.id
+                callback(null, req.body)
+            })
+        }, (data, callback) => {
+            Profile.findOneAndUpdate({id: req.user.id}, data, (err, profile) => {
+                if(err) { callback(err, null) }
+                delete profile._id
+                callback(null, profile)
+            })
+        }
+    ], (err, data) => {
+        if(err) { return res.status(400).send(err) }
+        return res.status(200).send(data)
     })
 })
-
-router.get("/:id", passport.authenticate("jwt", {session: false}), (req, res, next) => {
-    if(req.user.id == req.params.id){
-        ["_id", "password"].forEach(element => delete req.user[element])
-        return res.status(200).status(req.user)
-    }
-    Profile.findOne({id: req.param.id}, projections, (err, profile) => {
-        if(profile) { return res.status(200).send(profile) }
-        return res.status(400).send({error: err})
-    })
-})
-
 
 module.exports = router
