e06f75e29f6543dda8b37a2c8b3bfa4eda1bfe7c
Kevin Quach <kevin.quach@outlook.fr>
2019-07-02 11:24:46 +0200

    test:: prettier

--- routes/types.js
+++ routes/types.js
@@ -1,8 +1,8 @@
-const express = require("express")
-const config = require("../config/database")
+const express = require('express');
+const config = require('../config/database');
 
-const Type = require("../models/type")
-const router = express.Router()
+const Type = require('../models/type');
+const router = express.Router();
 
 // router.post("/new", (req, res, next) => {
 //     const type = new Type(req.body)
@@ -12,11 +12,23 @@ const router = express.Router()
 //     })
 // })
 
-router.get("/", (req, res, next) => {
-    Type.find({}, {_id:0, __v:0}, (err, types) => {
-        if(err) { return res.status(500).send(err) }
-        return res.status(200).send({data: types})
-    })
-})
+router.get('/', (req, res, next) => {
+  Type.find({}, { _id: 0, __v: 0 }, (err, types) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    return res.status(200).send({ data: types });
+  });
+});
 
-module.exports = router
+router.get('/:id', (req, res, next) => {
+  const projections = { _id: 0, password: 0, __v: 0 };
+  Type.findOne({ id: req.params.id }, projections, (err, profile) => {
+    if (err) {
+      res.status(404).send();
+    }
+    return res.status(200).send(profile);
+  });
+});
+
+module.exports = router;
